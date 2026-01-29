a9ec347deb2cbc65753190aa5845677801825b0e
perel <perelman.nir@gmail.com>
2026-01-29 12:24:40 +0200

    add test and content

--- src/web/calculator.py
+++ src/web/calculator.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from playwright.sync_api import Page, expect
 
+from web.elements import Inputbox, DropDownList
+
 
 class OperatorType(Enum):
     ADD = 0
@@ -12,51 +14,48 @@ class OperatorType(Enum):
 
 class CalculatorPage:
     def __init__(self, page, url):
-        self.page: Page = page
+        self._page: Page = page
         self.url = url
+        self.first_number = Inputbox(self._page,"#number1Field")
+        self.second_number = Inputbox(self._page,"#number2Field")
+        self.operator = DropDownList(self._page,"#selectOperationDropdown")
 
     def navigate(self):
-        self.page.goto(self.url)
-
-    def set_first_number(self, num: str):
-        self.page.locator("#number1Field").fill(num)
-
-    def set_second_number(self, num: str):
-        self.page.locator("#number2Field").fill(num)
-
-    def set_operator(self, operator: OperatorType):
-        self.page.locator("#selectOperationDropdown").select_option(index=operator.value)
+        self._page.goto(self.url)
 
     def click_calculate(self):
-        self.page.locator("#calculateButton").click()
+        self._page.locator("#calculateButton").click()
 
     def get_answer(self) -> str:
-        return self.page.locator("input[id='numberAnswerField']").input_value()
+        return self._page.locator("input[id='numberAnswerField']").input_value()
 
     def set_build(self, build_number: str):
-        self.page.locator("#selectBuild").select_option(value=build_number)
+        self._page.locator("#selectBuild").select_option(value=build_number)
 
     def wait_for_answer(self, expected_answer: str) -> bool:
         try:
-            expect(self.page.locator("input[id='numberAnswerField']")).to_have_value(expected_answer)
+            expect(self._page.locator("input[id='numberAnswerField']")).to_have_value(expected_answer)
             return True
         except Exception as e:
             print(f"Error waiting for answer: {e}")
             return False
 
     def get_error(self) -> str:
-        return self.page.locator("#errorMsgField").text_content()
+        return self._page.locator("#errorMsgField").text_content()
 
     def clear_all(self):
-        self.page.locator("input", has_text="Clear").click()
+        self._page.locator("input", has_text="Clear").click()
 
     def set_integer_only(self, to_select: bool):
-        self.page.locator("#integerSelect").set_checked(to_select)
+        self._page.locator("#integerSelect").set_checked(to_select)
+
+    def get_error_message(self):
+        self._page.locator("#errorMsgField").text_content()
 
     def check_answer(self, first_num, second_num, operator: OperatorType, expected_answer: str) -> bool:
-        self.set_first_number(first_num)
-        self.set_second_number(second_num)
-        self.set_operator(operator)
+        self.first_number = first_num
+        self.second_number = second_num
+        self.math_operator = operator
         self.click_calculate()
         return self.wait_for_answer(expected_answer)
 
--- src/web/elements.py
+++ src/web/elements.py
@@ -1,22 +1,38 @@
-# from playwright.async_api import Page
-#
-#
-# class ElementHandler:
-#     def __init__(self, page: Page):
-#         self.page: Page = page
-#
-#     def _get_by_class_name(self, class_name: str, text: str | None = None):
-#         element = self.page.locator(f".{class_name}")
-#         if text:
-#             return element.filter(has_text=text)
-#         return element
-#
-#     def click(self, element: str):
-#         self.page.locator(element).click()
-#
-#     def fill(self):
-#
-# class ElementFinder:
-#     def __init__(self, page: Page):
-#         self.page: Page = page
-#
+from playwright.sync_api import Page, expect
+from web.calculator import OperatorType
+
+
+class Inputbox:
+    def __init__(self, page: Page, locator: str):
+        self._locator: str = locator
+        self._page = page
+
+    @property
+    def value(self) -> str:
+        return self._page.locator(self._locator).input_value()
+
+    @value.setter
+    def value(self, num: str):
+        self._page.locator(self._locator).fill(num)
+
+    def wait_for_value(self, expected_answer: str) -> bool:
+        try:
+            expect(self._page.locator(self._locator)).to_have_value(expected_answer)
+            return True
+        except Exception as e:
+            print(f"Error waiting for answer: {e}")
+            return False
+
+
+class DropDownList:
+    def __init__(self, page: Page, locator: str):
+        self._locator: str = locator
+        self._page = page
+
+    @property
+    def value(self) -> str:
+        return self._page.locator("#selectOperationDropdown").input_value()
+
+    @value.setter
+    def value(self, operator: OperatorType):
+        self._page.locator("#selectOperationDropdown").select_option(index=operator.value)
