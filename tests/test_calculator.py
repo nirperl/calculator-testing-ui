a58280cbac16e5f3207301ed9bb46cf1eda76da6
perel <perelman.nir@gmail.com>
2026-02-01 17:31:44 +0200

    fixed conventions with ruff

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -18,7 +18,7 @@ POSITIVE_DATA = [
     ("12", "3", OperatorType.DIVIDE, "4"),
     ("2", "4", OperatorType.MULTIPLY, "8"),
     ("10", "5", OperatorType.SUBTRACT, "5"),
-    ("100", "25", OperatorType.CONCATENATE, "10025")
+    ("100", "25", OperatorType.CONCATENATE, "10025"),
 ]
 
 # NEGATIVE_DATA = [
@@ -27,27 +27,31 @@ POSITIVE_DATA = [
 #     ("1a")
 # ]
 
-BUILD_DATA = ["Prototype"] #, "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+BUILD_DATA = ["Prototype"]  # , "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 
 class TestCalculator:
     @pytest.mark.parametrize("build", BUILD_DATA)
     @pytest.mark.parametrize("first_num, second_num, operator, expected_answer", POSITIVE_DATA)
-    def test_positive_flow(self, browser, build, first_num, second_num, operator: OperatorType,
-                           expected_answer):
-        page_manager: PageManager = PageManager(browser,
-                                                "https://testsheepnz.github.io/BasicCalculator.html")
+    def test_positive_flow(
+        self, browser, build, first_num, second_num, operator: OperatorType, expected_answer
+    ):
+        page_manager: PageManager = PageManager(
+            browser, "https://testsheepnz.github.io/BasicCalculator.html"
+        )
         page_manager.initialize()
         page_manager.calculator.navigate()
         page_manager.calculator.build.value = build
-        assert page_manager.calculator.check_answer(first_num, second_num, operator,
-                                                    expected_answer)
-
+        assert page_manager.calculator.check_answer(
+            first_num, second_num, operator, expected_answer
+        )
 
     def test_wrong_values(self, browser):
         too_long_num = "11111111111"
         max_size_num = "1111111111"
-        page_manager: PageManager = PageManager(browser,
-                                                "https://testsheepnz.github.io/BasicCalculator.html")
+        page_manager: PageManager = PageManager(
+            browser, "https://testsheepnz.github.io/BasicCalculator.html"
+        )
         page_manager.initialize()
         page_manager.calculator.navigate()
         page_manager.calculator.first_number.value = too_long_num
